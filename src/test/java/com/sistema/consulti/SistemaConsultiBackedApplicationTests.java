ceeb7bcafcf50a2da35786567e670aca2489d559
Eduardo Vargas <eduardoevargasp@hotmail.com>
2023-01-22 19:03:03 -0500

    Proyecto Base

---
+++ src/test/java/com/sistema/consulti/SistemaConsultiBackedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.consulti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaConsultiBackedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
